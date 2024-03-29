cbd1dea34ad42c985b17c9f318b253e58954dd2e
mstp <EHStudent230@hotmail.com>
2024-03-29 19:26:40 -0400

    commit

--- poo_mysql/agence_com/main.py
+++ poo_mysql/agence_com/main.py
@@ -8,8 +8,10 @@ from departement.departement import Departement
 #data = EmployeDao.create(emp)
 #print(data)
 employes,message = EmployeDao.list_all()
-print(employes,message)
 
+for employe in employes:
+    print(f"Nom : {employe[0]}, Prénom : {employe[1]}, Matricule : {employe[2]}, Fonction : {employe[3]}, Département : {employe[4]}")
+    print("-----------------------------------------------------------------------------------")
 
 #dpt = Departement("IT","étage 2","John")
 #DepartementDao.create(dpt)

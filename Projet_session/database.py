7eda35007855b1467439ddefe459108d540d571d
mstp <EHStudent230@hotmail.com>
2024-04-12 18:06:02 -0400

    commit

--- Projet_session/database.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector as mysql
-
-def connexion_db():
-    return mysql.connect(
-        user='root',
-        password= '',
-        host='localhost',
-        database='projet_session'
-        )
